7b83e20dbdc428865096832196420790241bbe2e
WeDev <webdevjs2022@gmail.com>
2023-05-03 20:35:04 +0500

    3.5 Создание компонента ProductItem.vue

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,55 +1,15 @@
 <template>
     <ul class="catalog__list">
-        <li class="catalog__item" v-for="(product, index) in products" :key="index">
-            <a class="catalog__pic" href="#">
-                <img :src="product.image" :alt="product.title">
-            </a>
-
-            <h3 class="catalog__title">
-                <a href="#">
-                    {{ product.title }}
-                </a>
-            </h3>
-
-            <span class="catalog__price">
-                {{ product.price }} ₽
-            </span>
-
-            <ul class="colors colors--black">
-                <li class="colors__item">
-                    <label class="colors__label" for="input1">
-                        <input aria-labelledby="#input1" id="input1"
-                        class="colors__radio sr-only" type="radio"
-                            name="color-1" value="#73B6EA" checked="">
-                        <span class="colors__value" style="background-color: #73B6EA;">
-                        </span>
-                    </label>
-                </li>
-                <li class="colors__item">
-                    <label class="colors__label" for="input2">
-                        <input aria-labelledby="#input2" id="input2"
-                        class="colors__radio sr-only" type="radio"
-                            name="color-1" value="#8BE000">
-                        <span class="colors__value" style="background-color: #8BE000;">
-                        </span>
-                    </label>
-                </li>
-                <li class="colors__item">
-                    <label class="colors__label" for="input3">
-                        <input aria-labelledby="#input3" id="input3"
-                        class="colors__radio sr-only" type="radio"
-                            name="color-1" value="#222">
-                        <span class="colors__value" style="background-color: #222;">
-                        </span>
-                    </label>
-                </li>
-            </ul>
-        </li>
+        <ProductItem v-for="(product, index) in products" :key="index" :product="product"/>
     </ul>
 </template>
 
 <script>
+import ProductItem from './ProductItem.vue';
+
 export default {
+  name: 'ProductList',
   props: ['products'],
+  components: { ProductItem },
 };
 </script>
