0660044fe83d38ba4e504ba8da46bce371b2b7f2
WeDev <webdevjs2022@gmail.com>
2023-05-04 20:08:53 +0500

    4.3 Пагинация стрелки

--- src/components/BasePagination.vue
+++ src/components/BasePagination.vue
@@ -1,46 +1,54 @@
 <template>
 <ul class="catalog__pagination pagination">
           <li class="pagination__item">
-            <a class="pagination__link pagination__link--arrow pagination__link--disabled"
-            aria-label="Предыдущая страница">
+            <button class="pagination__link pagination__link--arrow"
+            :disabled="modelValue === 1" aria-label="Предыдущая страница"
+            @click.prevent="prevPage()">
               <svg width="8" height="14" fill="currentColor">
                 <use xlink:href="#icon-arrow-left"></use>
               </svg>
-            </a>
+            </button>
           </li>
           <li class="pagination__item" v-for="pageNumber in pages" :key="pageNumber">
             <a href="#" class="pagination__link"
-            :class="{'pagination__link--current': pageNumber === page}"
+            :class="{'pagination__link--current': pageNumber === modelValue}"
             @click.prevent="paginate(pageNumber)">
               {{ pageNumber }}
             </a>
           </li>
           <li class="pagination__item">
-            <a class="pagination__link pagination__link--arrow"
-            href="#" aria-label="Следующая страница">
+            <button class="pagination__link pagination__link--arrow"
+            :disabled="modelValue === 4" href="#" aria-label="Следующая страница"
+            @click.prevent="nextPage()">
               <svg width="8" height="14" fill="currentColor">
                 <use xlink:href="#icon-arrow-right"></use>
               </svg>
-            </a>
+            </button>
           </li>
         </ul>
 </template>
 
 <script>
 export default {
-  model: {
-    prop: 'page', // свойство
-    event: 'paginate', // событие
-  },
-  props: ['page', 'count', 'perPage'],
+//   model: {
+//     prop: 'modelValue', // свойство
+//     event: 'paginate', // событие
+//   },
+  props: ['modelValue', 'count', 'perPage'],
   computed: {
     pages() { // количество страниц
       return Math.ceil(this.count / this.perPage);
     },
   },
   methods: {
-    paginate(page) {
-      this.$emit('update:modelValue', page);
+    paginate(modelValue) {
+      this.$emit('update:modelValue', modelValue);
+    },
+    nextPage() {
+      this.$emit('update:modelValue', (this.modelValue + 1));
+    },
+    prevPage() {
+      this.$emit('update:modelValue', (this.modelValue - 1));
     },
   },
 };
--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue';
+import { createApp, h } from 'vue';
 import App from './App.vue';
+// import router from "./router";
+// import store from "./store";
 
-createApp(App).mount('#app');
+const app = createApp({
+  render: () => h(App),
+});
+
+// app.use(router);
+// app.use(store);
+app.mount('#app');
+
+// createApp(App).mount('#app');
