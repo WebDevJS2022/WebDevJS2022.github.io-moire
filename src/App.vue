07bef10b08736488acf54b0087df4bf40f7e735a
WeDev <webdevjs2022@gmail.com>
2023-05-08 08:49:30 +0500

    4.5 Фильтр по цене, категории; modelValue:

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,78 @@
 <template>
- <section class="catalog">
+  <main class="content container">
+    <div class="content__top">
 
-        <ProductList :products="products"/>
+      <div class="content__row">
+        <h1 class="content__title">
+          Каталог
+        </h1>
+        <span class="content__info">
+          152 товара
+        </span>
+      </div>
+    </div>
 
-        <BasePagination :modelValue="page" @update:modelValue="page = $event"
-        :count="countProducts" :per-page="productsPerPage"/>
+    <div class="content__catalog">
+      <ProductFilter v-model:price-from="filterPriceFrom"
+      v-model:price-to="filterPriceTo"
+      v-model:category-id="filterCategoryId"/>
+
+      <section class="catalog">
+        <ProductList :products="products" />
+        <BasePagination :modelValue="page" @update:modelValue="page = $event" :count="countProducts"
+          :per-page="productsPerPage" />
       </section>
+
+    </div>
+  </main>
 </template>
 
 <script>
 import ProductList from '@/components/ProductList.vue';
 import products from '@/data/products';
 import BasePagination from '@/components/BasePagination.vue';
+import ProductFilter from './components/ProductFilter.vue';
 
 export default {
   name: 'App',
   data() {
     return {
+      filterPriceFrom: 0,
+      filterPriceTo: 0,
+      filterCategoryId: 0,
+
       page: 1,
       productsPerPage: 3,
     };
   },
   computed: {
+    // вычисляемое свойство, кот будет возвращать отфильтрованный список товаров
+    filteredProducts() {
+      let filteredProducts = products;
+
+      if (this.filterPriceFrom > 0) {
+        filteredProducts = filteredProducts
+          .filter((product) => product.price > this.filterPriceFrom);
+      }
+      if (this.filterPriceTo > 0) {
+        filteredProducts = filteredProducts
+          .filter((product) => product.price < this.filterPriceTo);
+      }
+      if (this.filterCategoryId) {
+        filteredProducts = filteredProducts
+          .filter((product) => product.categoryId === this.filterCategoryId);
+      }
+      return filteredProducts;
+    },
+
     products() {
       const offset = (this.page - 1) * this.productsPerPage;
-      return products.slice(offset, offset + this.productsPerPage);
+      return this.filteredProducts.slice(offset, offset + this.productsPerPage);
     },
     countProducts() {
-      return products.length;
+      return this.filteredProducts.length;
     },
   },
-  components: { ProductList, BasePagination },
+  components: { ProductList, BasePagination, ProductFilter },
 };
 </script>
---
+++ src/components/ProductFilter.vue
@@ -0,0 +1,163 @@
+<template>
+    <aside class="filter">
+        <form class="filter__form form" action="#" method="get">
+            <fieldset class="form__block">
+                <legend class="form__legend">Цена</legend>
+                <label class="form__label form__label--price" for=" ">
+                    <input class="form__input" name="min-price"
+                    :value="priceFrom" @input="updateInput">
+                    <span class="form__value">От</span>
+                </label>
+                <label class="form__label form__label--price" for=" ">
+                    <input class="form__input" name="max-price"
+                    :value="priceTo" @input="updateInput2">
+                    <span class="form__value">До</span>
+                </label>
+            </fieldset>
+
+            <fieldset class="form__block">
+                <legend class="form__legend">Категория</legend>
+                <label class="form__label form__label--select" for=" ">
+                    <select class="form__select" name="category"
+                    :value="categoryId" @input="updateInput3">
+                        <option value="0">Все категории</option>
+                        <option :value="category.id" v-for="category in categories"
+                        :key="category.id">
+                            {{ category.title }}</option>
+                    </select>
+                </label>
+            </fieldset>
+
+            <fieldset class="form__block">
+                <legend class="form__legend">Материал</legend>
+                <ul class="check-list">
+                    <li class="check-list__item">
+                        <label class="check-list__label" for=" ">
+                            <input class="check-list__check sr-only"
+                            type="checkbox" name="material" value="лен">
+                            <span class="check-list__desc">
+                                лен
+                                <span>(3)</span>
+                            </span>
+                        </label>
+                    </li>
+                    <li class="check-list__item">
+                        <label class="check-list__label" for=" ">
+                            <input class="check-list__check sr-only"
+                            type="checkbox" name="material" value="хлопок">
+                            <span class="check-list__desc">
+                                хлопок
+                                <span>(46)</span>
+                            </span>
+                        </label>
+                    </li>
+                    <li class="check-list__item">
+                        <label class="check-list__label" for=" ">
+                            <input class="check-list__check sr-only"
+                            type="checkbox" name="material" value="шерсть">
+                            <span class="check-list__desc">
+                                шерсть
+                                <span>(20)</span>
+                            </span>
+                        </label>
+                    </li>
+                    <li class="check-list__item">
+                        <label class="check-list__label" for=" ">
+                            <input class="check-list__check sr-only"
+                            type="checkbox" name="material" value="шелк">
+                            <span class="check-list__desc">
+                                шелк
+                                <span>(30)</span>
+                            </span>
+                        </label>
+                    </li>
+                </ul>
+            </fieldset>
+
+            <fieldset class="form__block">
+                <legend class="form__legend">Коллекция</legend>
+                <ul class="check-list">
+                    <li class="check-list__item">
+                        <label class="check-list__label" for=" ">
+                            <input class="check-list__check sr-only"
+                            type="checkbox" name="collection" value="лето"
+                                checked="">
+                            <span class="check-list__desc">
+                                лето
+                                <span>(2)</span>
+                            </span>
+                        </label>
+                    </li>
+                    <li class="check-list__item">
+                        <label class="check-list__label" for=" ">
+                            <input class="check-list__check sr-only"
+                            type="checkbox" name="collection" value="зима">
+                            <span class="check-list__desc">
+                                зима
+                                <span>(53)</span>
+                            </span>
+                        </label>
+                    </li>
+                    <li class="check-list__item">
+                        <label class="check-list__label" for=" ">
+                            <input class="check-list__check sr-only"
+                            type="checkbox" name="collection" value="весна">
+                            <span class="check-list__desc">
+                                весна
+                                <span>(24)</span>
+                            </span>
+                        </label>
+                    </li>
+                    <li class="check-list__item">
+                        <label class="check-list__label" for=" ">
+                            <input class="check-list__check sr-only"
+                            type="checkbox" name="collection" value="осень">
+                            <span class="check-list__desc">
+                                осень
+                                <span>(30)</span>
+                            </span>
+                        </label>
+                    </li>
+                </ul>
+            </fieldset>
+
+            <button class="filter__submit button button--primery" type="submit">
+                Применить
+            </button>
+            <button class="filter__reset button button--second" type="button">
+                Сбросить
+            </button>
+        </form>
+    </aside>
+</template>
+
+<script>
+import categories from '@/data/categories';
+
+export default {
+  data() {
+    return {
+      currentPriceFrom: 0,
+      currentPriceTo: 0,
+      currentCategoryId: 0,
+    };
+  },
+  props: ['priceFrom', 'priceTo', 'categoryId'],
+  methods: {
+    updateInput(event) {
+      this.$emit('update:priceFrom', event.target.value);
+    },
+    updateInput2(event) {
+      this.$emit('update:priceTo', event.target.value);
+    },
+    updateInput3(event) {
+      this.$emit('update:categoryId', event.target.value);
+    },
+  },
+  computed: {
+    categories() {
+      return categories;
+    },
+  },
+};
+</script>
